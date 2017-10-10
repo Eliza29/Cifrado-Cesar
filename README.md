3283a1de8c9743a4294012217b4d6d229a1e0302
eliza29 <elizaseguraf@gmail.com>
2017-10-10 11:47:00 -0500

    cambios 1

--- README.md
+++ README.md
@@ -1,9 +1,33 @@
-# CIFRADO CESAR
+# PSEUDOCODIGO
+### Cifrado Cesar
 
-### Escribir "Escribe una frase"
-### Leer Frase;
-### Funcion cipher(phrase)
-### encryptedString= ''
-### Para  i=0 hasta longitud de frase hacer
-### Escribir "Escribe una frase"
-### Escribir "Escribe una frase"var encryptedString= ''
+INICIO  
+Hacer  
+(variable phrase =Escribir "Escribe una frase");  
+Mientras (!phrase || !isNaN(prase))  
+Fin Hacer;
+
+Funcion cipher(phrase)  
+variable: EncryptedString= vacio  
+Para  i=0 hasta longitud de phrase hacer  
+Si (phrase.charCodeAt([i])>= 65 && phrase.charCodeAt([i])<= 90 && phrase.charCodeAt([i])!=32)) Entonces  
+encryptedString+= String.fromCharCode((phrase.charCodeAt([i])-65+33)% 26+ 65);  
+Si no si (phrase.charCodeAt([i])>= 97 && phrase.charCodeAt([i])<= 122 && phrase.charCodeAt([i])!=32) Entonces  
+encryptedString+= String.fromCharCode((phrase.charCodeAt([i])-97+33)% 26+ 97);  
+Fin Si  
+retornar encryptedString     
+Fin Funcion
+
+Funcion decipher(phrase)  
+variables: newPhrase= cipher(phrase), decipherString= vacio;  
+Para  i=0 hasta longitud de newPhrase hacer  
+Si (newPhrase.charCodeAt([i])>= 65 && newPhrase.charCodeAt([i])<= 90 && phrase.charCodeAt([i])!=32) Entonces  
+  decipherString+= String.fromCharCode((newPhrase.charCodeAt([i])-65)-33%26+65);
+  Si no si (newPhrase.charCodeAt([i])>= 97 && newPhrase.charCodeAt([i])<= 122 && phrase.charCodeAt([i])!=32) Entonces  
+  decipherString+= String.fromCharCode((newPhrase.charCodeAt([i])-97)-33%26+97);  
+  Fin Si  
+  retornar decipherString;    
+  Fin Funcion  
+  FIN
+
+  [DIAGRAMA DE FLUJO] (https://drive.google.com/file/d/0Bz7FI76KT0SfNG9UZ18tTjViSUk/view?usp=sharing)
--- js/app.js
+++ js/app.js
@@ -1,38 +1,34 @@
 //Cifrado Cesar
-//Le pedimos al usuario que ingrese una frase mediante prompt.
+//El usuario no podra ingresar un campo vacío o que contenga números
 do {
   var phrase = prompt('Escribe una frase');
-} while(!phrase || !isNaN(phrase));//El usuario no podra ingresar un campo vacío o que contenga números
+} while(!phrase || !isNaN(phrase));
 //Creamos la funcion cipher para encriptar el mensaje
 function cipher(phrase){
-    var encryptedString= '';//Creamos una variable donde se va almacenar la frase encritada
-
-    for(var i=0; i < phrase.length; i++){//recorremos por todos los elementos de la frase
-    if (phrase.charCodeAt([i])>= 65 && phrase.charCodeAt([i])<= 90){//la condicion nos permite devolver mayusculas
-    encryptedString+= String.fromCharCode((phrase.charCodeAt([i])-65+33)% 26+ 65)
+  var encryptedString= '';//Creamos una variable donde se va almacenar la frase encritada
+  for(var i=0; i < phrase.length; i++){//recorremos por todos los elementos de la frase
+    if (phrase.charCodeAt([i])>= 65 && phrase.charCodeAt([i])<= 90 && phrase.charCodeAt([i])!=32){//la condicion nos permite devolver mayusculas
+      encryptedString+= String.fromCharCode((phrase.charCodeAt([i])-65+33)% 26+ 65);
       }//La formula hace que cada elemento de la frase recorra 33 espacios en el abecedario.
-    else if (phrase.charCodeAt([i])>= 97 && phrase.charCodeAt([i])<= 122){//la condicion nos permite devolver minusculas
-    encryptedString+= String.fromCharCode((phrase.charCodeAt([i])-97+33)% 26+ 97)
+    else if (phrase.charCodeAt([i])>= 97 && phrase.charCodeAt([i])<= 122 && phrase.charCodeAt([i])!=32){//la condicion nos permite devolver minusculas
+      encryptedString+= String.fromCharCode((phrase.charCodeAt([i])-97+33)% 26+ 97);
      }
     }
 return encryptedString;//Nos da la nueva frase encriptada
 }
-console.log (cipher(phrase));
-//Creamos la funcion decipher para decifrar el mensaje
-function decipher(phrase){
-
-    var newPhrase= cipher(phrase);
-    var decipherString= '';
-
-    for(var j=0; j < newPhrase.length; j++){
-    if (newPhrase.charCodeAt([j])>= 65 && newPhrase.charCodeAt([j])<= 90){
-    decipherString+= String.fromCharCode((newPhrase.charCodeAt([j])-65-33)% 26+65);
+function decipher(phrase){//Creamos la funcion decipher para decifrar el mensaje
+  var newPhrase= cipher(phrase);
+  var decipherString= '';
+  for(var i=0; i < newPhrase.length; i++){
+    if (newPhrase.charCodeAt([i])>= 65 && newPhrase.charCodeAt([i])<= 90 && phrase.charCodeAt([i])!=32){
+       decipherString+= String.fromCharCode((newPhrase.charCodeAt([i])-65)-33%26+65);
       }
-    else if (newPhrase.charCodeAt([j])>= 97 && newPhrase.charCodeAt([j])<= 122){
-    decipherString+= String.fromCharCode((newPhrase.charCodeAt([j])-97-33)% 26+97)
+    else if (newPhrase.charCodeAt([i])>= 97 && newPhrase.charCodeAt([i])<= 122 && phrase.charCodeAt([i])!=32){
+    decipherString+= String.fromCharCode((newPhrase.charCodeAt([i])-97)-33%26+97);
      }
     }
 return decipherString;
 }
-
+console.log(phrase);
+console.log (cipher(phrase));
 console.log (decipher(phrase));
